bbc2d9b14bdfeb438f3e8192db1a1aac17f2af1d
Joezari Borlongan <bor.joezari@gmail.com>
2025-08-25 21:24:31 -0700

    build: add Mockito dependency for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
 }
 
 tasks.test {
