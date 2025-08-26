48e255035a34702850f21d82a77df5ed0b6ab22b
Joezari Borlongan <bor.joezari@gmail.com>
2025-08-25 21:25:06 -0700

    feat: add DI for more thorough testing

--- src/main/java/com/mastermind/services/RandomNumberApiClient.java
+++ src/main/java/com/mastermind/services/RandomNumberApiClient.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -14,20 +15,25 @@ import java.util.regex.Pattern;
 public class RandomNumberApiClient {
     private static final String INTEGER_GEN_URI = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
 
-    private final HttpClient CLIENT;
+    private final HttpClient client;
 
     public RandomNumberApiClient() {
-        this.CLIENT = HttpClient.newHttpClient();
+        this.client = HttpClient.newHttpClient();
+    }
+
+    RandomNumberApiClient(HttpClient client) {
+        this.client = client;
     }
 
     public NumCombination getRandomNums() {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(INTEGER_GEN_URI))
                 .GET()
+                .timeout(Duration.ofSeconds(10))
                 .build();
 
         try {
-            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             // Split body on every new line (Ref: https://www.random.org/clients/http/api/)
             String[] lines = response.body()
